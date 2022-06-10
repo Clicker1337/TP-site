06de68ac558d34e52694784451785de71fea5a98
Clicker1337 <lolololo.lol@list.ru>
2022-06-10 07:48:05 +0700

    post comment

--- src/pages/SaladRecipes/SaladRecipePage.tsx
+++ src/pages/SaladRecipes/SaladRecipePage.tsx
@@ -5,6 +5,7 @@ import { useParams, useNavigate } from 'react-router-dom'
 import s from './SaladRecipePage.module.scss'
 import Comments from "./Comments/CommentItem"
 import { GlobalPngSelector } from "../../assets/images/GlobalPngSelector"
+import { valueTernary } from "react-select/dist/declarations/src/utils"
 
 
 export const SaladRecipePage: FC = () => {
@@ -42,12 +43,26 @@ export const SaladRecipePage: FC = () => {
 
   globalThis.recipeID = Number(params.id)
 
+  async function fetchInfo() {
+    var text = document.getElementsByTagName("input")[0];
+    var val = text.value;
+    try {
+      const response = await axios.post('url', {
+        text: val,
+        id: params.id
+      })
+    } catch (e) {
+      alert(e)
+    }
+
+  }
+
   return (
     <div className={s.container}>
       <div className={s.box}>
         <div className={s.upperside}>
           <div className={s.leftside}>
-          <button className={s.button} onClick={() => navigate('/salad')}> Назад </button>
+            <button className={s.button} onClick={() => navigate('/salad')}> Назад </button>
             <div className={s.title1}>
               Страница {recipe?.name}
             </div>
@@ -64,6 +79,9 @@ export const SaladRecipePage: FC = () => {
           </div>
         </div>
         <div className={s.bottomside}>
+          <div className={s.inputbox}><input className={s.input} placeholder="Напишите ваш комментарий"></input>
+            <button className={s.button} onClick={fetchInfo}>Отправить</button>
+          </div>
           <div className={s.comments}> {comments.map(comment => <Comments comment={comment} />)} </div>
         </div>
       </div>
